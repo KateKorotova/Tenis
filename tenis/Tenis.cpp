a9b3198cbf8923bade7d0cebce096edd880cf9b7
katya_000 <katya_000@KATEKOROTOVA>
2017-10-29 00:22:12 +0300

    tennis for two figures

--- tenis/Tenis.cpp
+++ tenis/Tenis.cpp
@@ -12,8 +12,14 @@ int rand_() {
 	return k; 
 }
 
+Figure::Figure(int up, int down, int right, int left) {
+	this->up = up;
+	this->down = down;
+	this->right = right;
+	this->left = left; 
+}
 
-void Ball::tenis(int start_x, int start_y) {
+void Figure::tenis(int start_x, int start_y) {
 	int swt1 = rand_();
 	int swt2 = rand_();
 
@@ -22,57 +28,60 @@ void Ball::tenis(int start_x, int start_y) {
 		start_x = start_x + swt1;
 		start_y = start_y + swt2;
 
-		if (start_x > width) {
-			swt1 = (-1)*abs(rand_());
-			swt2 = rand_();
-			start_x -= (start_x%width);
+		if (start_x + right > width) {
+			swt1 = (-1)*swt1;
+			start_x = width - right;
 		}
-		if (start_x < 0) {
-			swt1 = abs(rand_());
-			swt2 = rand_();
-			start_x = 0;
+		if (start_x - left < 0) {
+			swt1 = (-1)*swt1;
+			start_x = left;
 		}
 
-		if (start_y > height) {
-			swt2 = (-1)*abs(rand_());
-			swt1 = rand_();
-			start_y -= (start_y%height);
+		if (start_y+ down> height) {
+			swt2 = (-1)*swt2;
+			start_y = height -down;
 		}
 
-		if (start_y < 0) {
-			swt2 = abs(rand_());
-			swt1 = rand_();
-			start_y = 0;
+		if (start_y - up< 0) {
+			swt2 = (-1)*swt2;
+			start_y = up;
 		}
 
-		printat(start_x, start_y, "O");
+		paint(start_x, start_y);
 		pause(400);
 		system("cls");
 
 	}
 }
+ 
 
-void Figure1::paint() {
-	cout << "  " << "*" << endl;
-	cout << "*" << "   " << "*" << endl;
-	cout << " " << "*" << " " << "*" << endl;
+Figure1::Figure1():Figure(1, 1, 2, 2) {};
+
+Figure2::Figure2() :Figure(2, 1, 1, 1) {};
+
+
+void Figure1::paint(int x, int y) {
+	printat(x, y-1, "*");
+	printat(x - 2, y, "*");
+	printat(x + 2, y, "*");
+	printat(x - 1, y + 1, "*");
+	printat(x + 1, y + 1, "*");
 }
 
-void Figure2::paint() {
-	cout << "***" << endl;
-	cout << "*" << " " << "*" << endl;
-	cout << "***" << endl;
+void Figure2::paint(int x, int y) {
+	printat(x-1, y - 2, "*");
+	printat(x, y - 2, "*"); 
+	printat(x + 1, y - 2, "*");
+	printat(x-1, y-1, "*");
+	printat(x + 1, y-1, "*");
+	printat(x - 1, y, "*");
+	printat(x + 1, y, "*");
+	printat(x - 1, y + 1, "*");
+	printat(x, y + 1, "*");
+	printat(x + 1, y + 1, "*");
 }
 
-void Ball::paint() {
-	cout << "  " << "***" << "  " << endl;
-	cout << " " << "*" << "   " << "*" << endl;
-	cout << "*" << "     " << "*" << endl;
-	cout << "*" << "     " << "*" << endl;
-	cout << " " << "*" << "   " << "*" << endl;
-	cout << "  " << "***" << "  " << endl;
 
-}
 
 
 int main() {
@@ -80,34 +89,30 @@ int main() {
 	cout << "Choice:" << endl;
 	cout << "1. Paint figure 1" << endl;
 	cout << "2. Paint figure 2" << endl;
-	cout << "3. Paint ball" << endl;
-	cout << "4. Turn on tennis" << endl; 
+	cout << "3. Tennis 1" << endl;
+	cout << "4. Tennis 2" << endl;
 	int ch;
 	cin >> ch;
 	if (!cin.fail()) {
 		switch (ch) {
 		case 1:
 			figure = new Figure1();
-			figure->paint();
+			figure->paint(8, 10);
 			system("pause");
 			break;
 		case 2:
 			figure = new Figure2();
-			figure->paint();
+			figure->paint(8, 10);
 			system("pause");
 			break;
 		case 3:
-			figure = new Ball();
-			figure->paint();
-			system("pause");
+			figure = new Figure1();
+			figure->tenis(6, 9);
 			break;
 		case 4:
-			Ball a;
-			a.tenis(17, 21);
-			system("cls");
-			waitAnyKey();
+			figure = new Figure2();
+			figure->tenis(6, 9);
 			break;
 		}
 	}
-
 }
--- tenis/Tenis.h
+++ tenis/Tenis.h
@@ -7,25 +7,30 @@ using namespace std;
 
 
 class Figure {
+protected:
+	const int height = 30;
+	const int width = 30;
+	int up;
+	int down;
+	int left;
+	int right;
 public:
-	void virtual paint() = 0;
+	Figure(int up, int down, int right, int left);
+	void virtual paint(int x, int y) = 0;
+	void tenis(int start_x, int start_y);
+
 };
 
 class Figure1 : public Figure {
 public:
-	void paint();
+	Figure1();
+	void paint(int x, int y);
 };
 
 class Figure2 : public Figure {
 public:
-	void paint();
+	Figure2();
+	void paint(int x, int y);
+
 };
 
-class Ball : public Figure {
-private:
-	const int height = 30;
-	const int width = 30;
-public:
-	void tenis(int start_x, int start_y);
-	void paint();
-};
